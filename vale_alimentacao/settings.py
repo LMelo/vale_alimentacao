211f7e1e5ccc41fc2c23369f53af58a87b21ad0a
Luis <luis.nickelback@gmail.com>
2014-03-20 14:45:03 -0300

    Update settings.py

--- vale_alimentacao/settings.py
+++ vale_alimentacao/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -86,6 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT_PATH, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "vale_alimentacao/bootstrap"),
+    os.path.join(PROJECT_ROOT_PATH, 'bootstrap'),
 )
